102dc231f43bdbcc7179db49e0d578dfecdcd067
Wallekson <walisoncastor@gmail.com>
2023-07-24 06:48:57 -0300

    DateTimeFormatter

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
@@ -14,6 +15,9 @@ public class Program {
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); // systemDefault define a data-hora a partir fuso do a usuario
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+        DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
 
         LocalDate d01 = LocalDate.now(); //Data local
         LocalDateTime d02 = LocalDateTime.now(); // Data-hora local
@@ -31,6 +35,7 @@ public class Program {
         LocalDate d10 = LocalDate.of(2022,7,20);
         LocalDateTime d11 = LocalDateTime.of(2022,7,20,1,30);
 
+        System.out.println("----AULA INSTANCIAÇÃO----");
 
         System.out.println("d01 = " + d01.toString());
         System.out.println("d02 = " + d02.toString());
@@ -43,6 +48,24 @@ public class Program {
         System.out.println("d09 = " + d09.toString());
         System.out.println("d10 = " + d10.toString());
         System.out.println("d11 = " + d11.toString());
+
+        //AULA FORMATACAO -Transformar data-hora em texto
+
+        System.out.println("----AULA FORMATAÇÃO----");
+
+        System.out.println("d04 = " + d04.format(fmt1)); // instanciamos o DateTimeFormatter como argumento
+        System.out.println("d04 = " + fmt1.format(d04));
+        System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))); //instancia apenas quando roda o arquivo
+
+        System.out.println("d05 = " + d05.format(fmt1));
+        System.out.println("d05 = " + d05.format(fmt2));
+        System.out.println("d05 = " + d05.format(fmt4));
+
+
+        System.out.println("d06 = " + fmt3.format(d06));
+        System.out.println("d06 = " + fmt5.format(d06)); //Instant precisa de um formato pré definido especifico com fuso
+        System.out.println("d06 = " + d06.toString());
+
 
     }
 }
